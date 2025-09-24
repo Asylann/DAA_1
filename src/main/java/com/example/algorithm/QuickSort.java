97cc276f762d2f70b0de796b748bec4b4990b0b7
Asylann <usenasylan@gmail.com>
2025-09-24 13:33:13 +0500

    Refactoring utils such as swap and partition

--- src/main/java/com/example/algorithm/QuickSort.java
+++ src/main/java/com/example/algorithm/QuickSort.java
@@ -1,6 +1,7 @@
 package com.example.algorithm;
 
 import com.example.util.Metrics;
+import com.example.util.SortUtils;
 
 import java.util.Random;
 
@@ -19,10 +20,10 @@ public class QuickSort {
         while (start < end) {
             // random pivot selecting
             int pivotIndex = start + random.nextInt(end - start + 1);
-            swap(arr, pivotIndex, end);
+            SortUtils.swap(arr, pivotIndex, end);
 
             // partition around random pivot
-            int p = partition(arr, start, end);
+            int p = SortUtils.partition(arr, start, end);
             if (p - start < end - p) {
                 metrics.enterRecursion();
                 quickSort(arr, start, p - 1, metrics);
@@ -35,26 +36,5 @@ public class QuickSort {
                 end = p - 1;
             }
         }
-    }
-
-    private int partition(int[] arr, int start, int end) {
-        int pivot = arr[end];
-        int i = start - 1;
-
-        for (int j = start; j < end; j++) {
-            if (arr[j] <= pivot) {
-                i++;
-                swap(arr, i, j);
-            }
-        }
-
-        swap(arr, i + 1, end);
-        return i + 1;
-    }
-
-    private void swap(int[] arr, int a, int b) {
-        int temp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = temp;
     }
 }
---
+++ src/main/java/com/example/util/SortUtils.java
@@ -0,0 +1,24 @@
+package com.example.util;
+
+public class SortUtils {
+    public static int partition(int[] arr, int start, int end) {
+        int pivot = arr[end];
+        int i = start - 1;
+
+        for (int j = start; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+
+        swap(arr, i + 1, end);
+        return i + 1;
+    }
+
+    public static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
